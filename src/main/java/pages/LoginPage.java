03a8acaf12f896ec0b682aebb9cf08bf1ebd5323
JulyShev <uliasevcuk1452@gmail.com>
2026-01-20 00:25:14 +0500

    HW3

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,41 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+public class LoginPage extends BasePage{
+    public LoginPage(WebDriver driver){
+        setDriver(driver);
+        driver.get("https://ilcarro.web.app/login?url=%2Fsearch");
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver,10),this);
+    }
+    @FindBy(xpath = "//input[@id='email']")
+    WebElement fieldEmail;
+
+    public void inputEmail(){
+        fieldEmail.sendKeys("dornb12@mail.com");
+    }
+    @FindBy(xpath = "//input[@id='password']")
+    WebElement fieldPassword;
+
+    public void inputPassword(){
+        fieldPassword.sendKeys("Qwert123#1");
+    }
+
+    @FindBy(xpath = "//button[text()='Y’alla!']")
+    WebElement btnYalla;
+
+    public void clickBtnYalla(){
+        btnYalla.click();
+    }
+
+    @FindBy(xpath = "//button[text()='Ok']")
+    WebElement btnOk;
+
+    public void clickBtnOk(){
+        btnOk.click();
+    }
 }
--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -1,4 +1,24 @@
 package ui_tests;
 
-public class LoginTests {
+import manager.AppManager;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends AppManager {
+
+    @Test
+    public void loginPositiveTest(){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.inputEmail();
+        loginPage.inputPassword();
+        loginPage.clickBtnYalla();
+        loginPage.clickBtnOk();
+    }
+
+
 }
